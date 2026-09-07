5c4bdb351e2fa650e75fc7387dda48797780a314
nanyasg <nany.asg10@gmail.com>
2026-09-07 09:51:56 -0300

    colecciones

---
+++ src/Colecciones.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Colecciones {
+    public static void main(String[] args) {
+        
+        System.out.println("Todo impeke");
+        Scanner sc = new Scanner(System.in);
+
+        boolean mostrar_menu = true;
+        while(mostrar_menu) {
+            System.err.println("1. Conociendo Arrays");
+            System.err.println("2.Conociendo Arraylist");
+            System.err.println("3.Salir");
+
+            System.out.println("Ingrese una opcion");
+            String opcion_ingresada = sc.nextLine();
+
+            switch (opcion_ingresada){
+                case "1":
+                    conociendoArrays();
+                    break;
+                case "2":
+                    conociendoArraylist();
+                case "3":
+                    break;
+            }}
+        }
+        static void conociendoArrays(){
+            String[] alumnos = new String[4];
+        alumnos[0] = "Diego Marin";
+        alumnos[1] = "Amaro Lopez";
+        alumnos[2] = "Marianny Sanchez";
+        alumnos[3] = "Gustavo Araneda";
+
+        Integer[] edades = new Integer[4];
+        edades[0] = 18;
+        edades[1] = 19;
+        edades[2] = 19;
+        edades[3] = 18;
+
+        System.out.println("Datos de " + alumnos[0] + " edad: " + edades[0]);
+        System.out.println("Datos de " + alumnos[1] + " edad: " + edades[0]);
+        System.out.println("Datos de " + alumnos[2] + " edad: " + edades[0]);
+        System.out.println("Datos de " + alumnos[3] + " edad: " + edades[0]);
+
+        for (int i = 0; i < edades.length; i++) {
+            System.out.println("Datos de " + alumnos[i] + " edad: " + edades[i]);
+        }
+        
+    }
+
+    static void conociendoArraylist(){
+        ArrayList<String> alumnos = new ArrayList<>();
+
+alumnos.add("Diego Marin");
+alumnos.add("Amaro Lopez");
+alumnos.add("Marianny Sanchez");
+alumnos.add("Gustavo Araneda");
+
+alumnos.set(1, "Amaro López");
+alumnos.remove(1);
+System.err.println(alumnos.get(2));
+for (int i = 0; i < alumnos.size(); i++) {
+            System.out.println(alumnos.get(i));
+        }
+    }
+}
